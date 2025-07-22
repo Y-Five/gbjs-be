72649cec2a1fb8a7eba80caebbc38838c9dae55b
uni-j-uni <unijun0109@gmail.com>
2025-07-22 13:13:19 +0900

    :sparkles:Feat: JpaAuditing 설정 추가

--- src/main/java/com/yfive/gbjs/GbjsApplication.java
+++ src/main/java/com/yfive/gbjs/GbjsApplication.java
@@ -6,7 +6,9 @@ package com.yfive.gbjs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class GbjsApplication {
