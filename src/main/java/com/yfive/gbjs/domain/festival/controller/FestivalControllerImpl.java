10b76382802b62d6833df2f12836982abd148203
uni-j-uni <unijun0109@gmail.com>
2025-08-09 18:39:35 +0900

    :recycle:Refactor: 축제 관련 API 리팩토링

--- src/main/java/com/yfive/gbjs/domain/festival/controller/FestivalControllerImpl.java
+++ src/main/java/com/yfive/gbjs/domain/festival/controller/FestivalControllerImpl.java
@@ -3,15 +3,21 @@
  */
 package com.yfive.gbjs.domain.festival.controller;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.yfive.gbjs.domain.festival.dto.response.FestivalListResponse;
+import com.yfive.gbjs.domain.festival.dto.response.FestivalDetailResponse;
+import com.yfive.gbjs.domain.festival.dto.response.FestivalResponse;
 import com.yfive.gbjs.domain.festival.service.FestivalService;
+import com.yfive.gbjs.global.common.exception.PageErrorStatus;
 import com.yfive.gbjs.global.common.response.ApiResponse;
+import com.yfive.gbjs.global.common.response.PageResponse;
+import com.yfive.gbjs.global.error.exception.CustomException;
 
-import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,13 +29,26 @@ public class FestivalControllerImpl implements FestivalController {
   private final FestivalService festivalService;
 
   @Override
-  public ResponseEntity<ApiResponse<FestivalListResponse>> getFestivalsByRegion(
-      @Parameter(description = "지역", example = "안동시") @RequestParam String region,
-      @Parameter(description = "첫 번째 인덱스 값", example = "4") @RequestParam Integer startIndex,
-      @Parameter(description = "페이지 크기", example = "3") @RequestParam Integer pageSize) {
+  public ResponseEntity<ApiResponse<PageResponse<FestivalResponse>>> getFestivalsByRegion(
+      @RequestParam String region, @RequestParam Integer pageNum, @RequestParam Integer pageSize) {
 
-    FestivalListResponse response =
-        festivalService.getFestivalsByRegion(region, startIndex, pageSize);
+    if (pageNum < 1) {
+      throw new CustomException(PageErrorStatus.PAGE_NOT_FOUND);
+    }
+
+    Pageable pageable = PageRequest.of(pageNum - 1, pageSize);
+    PageResponse<FestivalResponse> festivalListResponse =
+        festivalService.getFestivalsByRegion(region, pageable);
+
+    return ResponseEntity.ok(ApiResponse.success(festivalListResponse));
+  }
+
+  @Override
+  public ResponseEntity<ApiResponse<FestivalDetailResponse>> getFestivalById(
+      @PathVariable(name = "festival-id") String id) {
+
+    FestivalDetailResponse response = festivalService.getFestivalById(id);
+
     return ResponseEntity.ok(ApiResponse.success(response));
   }
 }
