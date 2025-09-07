f76b8463f65c80da6ebe02b58798a008e5fc0699
uni-j-uni <unijun0109@gmail.com>
2025-09-07 13:06:39 +0900

    :recycle:Refactor: 코드 포맷 적용

--- src/main/java/com/yfive/gbjs/domain/chat/controller/ChatController.java
+++ src/main/java/com/yfive/gbjs/domain/chat/controller/ChatController.java
@@ -3,15 +3,18 @@
  */
 package com.yfive.gbjs.domain.chat.controller;
 
-import com.yfive.gbjs.domain.chat.dto.request.ChatRequest;
-import com.yfive.gbjs.global.common.response.ApiResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.yfive.gbjs.domain.chat.dto.request.ChatRequest;
+import com.yfive.gbjs.global.common.response.ApiResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "챗봇", description = "챗봇 관련 API (OpenAI)")
 @RequestMapping("/api/chat")
