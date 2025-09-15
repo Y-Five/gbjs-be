0bbd4a532bb641a66592a8d1c6d770a7422f7d5f
uni-j-uni <unijun0109@gmail.com>
2025-09-15 11:28:00 +0900

    :wrench:Settings: Endpoint Security 수정

--- src/main/java/com/yfive/gbjs/global/config/SecurityConfig.java
+++ src/main/java/com/yfive/gbjs/global/config/SecurityConfig.java
@@ -139,9 +139,11 @@ public class SecurityConfig {
         auth ->
             auth.requestMatchers("/swagger-ui/**", "/v3/api-docs/**")
                 .hasRole("DEVELOPER")
+                .requestMatchers(RegexRequestMatcher.regexMatcher(".*/dev.*"))
+                .hasRole("DEVELOPER")
                 .requestMatchers(
-                    "/api/auth/**",
                     "/actuator/health",
+                    "/api/auth/**",
                     "/api/seals/user",
                     "/api/seals/popular-spots",
                     "/api/seals/products",
@@ -157,8 +159,6 @@ public class SecurityConfig {
                 .permitAll()
                 .requestMatchers("/error")
                 .permitAll()
-                .requestMatchers(RegexRequestMatcher.regexMatcher(".*/dev.*"))
-                .hasRole("DEVELOPER")
                 .anyRequest()
                 .authenticated());
   }
